1d548aae709d3db47b326ea106adc36c0505bf45
Artem <arteam_101@mail.ru>
2022-04-01 00:53:35 +0700

    Добавьте файлы проекта.

---
+++ KanbanBoard/Models/Task.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Models
+{
+    public class Task: INotifyPropertyChanged
+    {
+        string header;
+        string description;
+        string? filePath;
+        
+        public string Header
+        {
+            get
+            {
+                return this.header;
+            }
+            set
+            {
+                this.header = value;
+                RaisePropertyChangedEvent("Header");
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+                RaisePropertyChangedEvent("Description");
+            }
+        }
+        public string? FilePath
+        {
+            get
+            {
+                return this.filePath;
+            }
+            set
+            {
+                this.filePath = value;
+                RaisePropertyChangedEvent("FilePath");
+            }
+        }
+
+        public Task(string header)
+        {
+            this.Header = header;
+        }
+
+        public void UploadFile(string path)
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+
+    }
+}
